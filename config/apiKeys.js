6ed6a5baead79e36aa29b16a6678242475785316
Cory Wilson <ctwilson08@gmail.com>
2015-10-11 20:13:14 -0400

    making some things visible

---
+++ config/apiKeys.js
@@ -0,0 +1,18 @@
+//File Name: ./app/config/apiKeys.js
+
+module.exports = {
+
+    'sportsradarNFL' : {
+      'key'          : 'dn99tfdpkq8eyu836hjc2pz9'
+    },
+    'sportsradarEPL' : {
+      'key'          : 'vgpmmwsd4jvmps27nrc9h2fv'
+    },
+    'footballData'   : {
+      'key'          : 'c162cb6d7e56493d934b712f58e282f4'
+    },
+    'StatsFC'        : {
+      'key'          : '_rTEZZsI666qy0yAOslDCb8yhlUuRBDwYjiC9q6a'
+    }
+
+};
