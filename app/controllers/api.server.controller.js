d7d425c365f737db79139302cf12828d72d9ed12
Cory Wilson <ctwilson08@gmail.com>
2015-11-17 16:55:26 -0500

    Grooming the game information pulled from the api. Aka converting unix time to readable

--- app/controllers/api.server.controller.js
+++ app/controllers/api.server.controller.js
@@ -1,22 +1,39 @@
 //File Name: ./app/controllers/api.server.controller.js
-var schedule = require('../../config/schedule'),
-    request = require('request');
+var moment   = require('moment'),
+    schedule = require('../../config/schedule'),
+    request  = require('request');
 
 module.exports = function(){
 
   var _readNFLAPI = function(req,res){
     //NFL schedule
+    var gameArray = [];
+
     for (var i = 0; i < schedule.length; i++) {
       var weekNum = i+1;
+      var url = 'https://profootballapi.com/schedule?api_key='+process.env.NFL_API_KEY+'&year=2015&week='+weekNum+'&season_type=REG';
       if (schedule[i] === true) {
         request
-         .post({url:'https://profootballapi.com/schedule?api_key='+process.env.NFL_API_KEY+'&year=2015&week='+weekNum+'&season_type=REG'},
+         .post({url:url},
            function(err, httpResponse, body){
-             if(err){
-               console.log(err);
-             }
-             res.send(body);
-           }
+            var data = JSON.parse(body);
+            for (var j = 0; j < data.length; j++) {
+              var game        = data[j];
+              var id          = game.id;
+              var home        = game.home;
+              var away        = game.away;
+              var day         = moment.unix(game.time).format('ddd');
+              var date        = moment.unix(game.time).format('MMM D');
+              var time        = moment.unix(game.time).format('h:mm a');
+              var week        = game.week;
+              var season_type = game.season_type;
+              var final       = game.final;
+              var home_score  = game.home_score;
+              var away_score  = game.away_score;
+              gameArray.push({id:id,home:home,away:away,day:day,date:date,time:time,week:week,season_type:season_type,final:final,home_score:home_score,away_score:away_score});
+            }
+            res.json(gameArray);
+          }
         );
       }
     }
--- app/routes/api.server.routes.js
+++ app/routes/api.server.routes.js
@@ -4,10 +4,10 @@ var usersController = require('../controllers/users.server.controller');
 
 module.exports = function(app) {
 
-    app.get('/nfl', usersController.requiresAuth, apiController.readNFLAPI);
+    app.get('/nfl', usersController.requiresAuth,  apiController.readNFLAPI);
 
     app.get('/nfl/:game_id', usersController.requiresAuth, apiController.readNFLGame);
-    
+
     app.get('/soccer/:week',apiController.readSoccerAPI);
 
 };
